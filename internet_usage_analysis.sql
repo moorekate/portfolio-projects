943973c59d6172f20e9ea5eaddd5a014e3c4dea7
Kate Alice Moore <90464545+moorekate@users.noreply.github.com>
2024-01-15 18:03:40 -0500

    Added questions

--- internet_usage_analysis.sql
+++ internet_usage_analysis.sql
@@ -1,2 +1,6 @@
 -- Kaggle dataset: https://www.kaggle.com/datasets/divyanshukunwar/internet-usage-analysis
+-- 1. What time of day is most active?
+-- 2. How often does the ip change?
+-- 3. What is average usage per hour, per day, per month?
+-- 4. Do different users use the same device?
 
